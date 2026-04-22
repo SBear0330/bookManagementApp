53b36333048d17901eff733ae639dc4136e8a267
Seongmin <ksm_2094@naver.com>
2026-04-23 01:54:13 +0900

    book list print form change: print title instead of obj num

--- bookManagementApp/models.py
+++ bookManagementApp/models.py
@@ -4,5 +4,8 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     authour = models.CharField(max_length=100)
     is_borrowed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title() # admin에서 book list를 출력할 때 default 객체 번호가 아닌 객체의 title을 출력
         
 # Create your models here.
